2e4bb26968a50aa35824be9ed4a65751708a6085
isllas.curty <isllasrezende@yahoo.com.br>
2024-07-10 13:28:14 -0300

    UPDATE

---
+++ Frameworks/Singleton/Padroes_De_Projeto_Java/src/one/digitalinnovation/gof/Strategy/Comportamento.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.gof.Strategy;
+
+public interface Comportamento {
+
+    void mover();
+
+}
--- Frameworks/Singleton/Padroes_De_Projeto_Java/src/one/digitalinnovation/gof/Test.java
+++ Frameworks/Singleton/Padroes_De_Projeto_Java/src/one/digitalinnovation/gof/Test.java
@@ -1,11 +1,20 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.Singleton.SingletonEager;
+import one.digitalinnovation.gof.Singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.Singleton.Singleton_Lazy;
+import one.digitalinnovation.gof.Strategy.Comportamento;
+import one.digitalinnovation.gof.Strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.Strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.Strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.Strategy.Robo;
+
 public class Test {
 
     public static void main(String[] args) {
 
         //Testes relacionados ao Design Patter Singleton.
-        
+
         Singleton_Lazy lazy = Singleton_Lazy.getInstancia();
         System.out.println(lazy);
         lazy = Singleton_Lazy.getInstancia();
@@ -20,6 +29,25 @@ public class Test {
         System.out.println(holder);
         holder = SingletonLazyHolder.getInstancia();
         System.out.println(holder);
+
+        //Strategy
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+        Comportamento normal = new ComportamentoNormal();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
 
     }
 
